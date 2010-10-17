777fa2ccfb08483ce072df6c3042480dfb7b0f44
Mason Simon <masonsimon@gmail.com>
2010-10-16 22:37:38 -0400

    added tests for Line.vertical?

---
+++ test/line/vertical_test.rb
@@ -0,0 +1,18 @@
+require 'test/unit'
+require 'geometry'
+
+class VerticalTest < Test::Unit::TestCase
+  include Geometry
+
+  def test_vertical
+    x, y = 0, 0
+    l1 = Line.new_by_arrays([x, y], [x, y+1])
+    assert l1.vertical?
+  end
+
+  def test_not_vertical
+    x, y = 0, 0
+    l1 = Line.new_by_arrays([x, y], [x+1, y+1])
+    assert ! l1.vertical?
+  end
+end
